3ff17912eb13f1ec22e6ec46ca6e0d491c41948e
James Carthew <james@James-Work>
2018-07-02 04:09:31 +1000

    Update to codebase.

---
+++ vegastrike/src/gldrv3/gl_pick.cpp
@@ -0,0 +1,76 @@
+/* GL selection buffer code
+ */
+
+//#include <GL/gl.h>
+#include "gl_globals.h"
+#include "gfxlib.h"
+#include "gl_matrix.h"
+#include <GL/glu.h>
+#include  <assert.h>
+#define SELECTBUF_SIZE MAX_PICK*4
+const float     epsilon = 0.001;
+
+static vector< PickData > *picked_objects = new vector< PickData > ();
+static unsigned selectbuf[SELECTBUF_SIZE];
+
+using namespace GFXMatrices;
+
+void GFXBeginPick( int x, int y, int xsize, int ysize )
+{
+    //save and change gl projection matrix
+    GLint viewport[4];
+
+    float left, right, bottom, top, fnear, ffar;
+    GFXGetFrustumVars( true, &left, &right, &bottom, &top, &fnear, &ffar );
+
+    glMatrixMode( GL_PROJECTION );
+    glPushMatrix();
+    glLoadIdentity();
+    glGetIntegerv( GL_VIEWPORT, viewport );
+    gluPickMatrix( x, viewport[3]-y, xsize, ysize, viewport );
+    glMultMatrixf( projection );
+
+    glSelectBuffer( SELECTBUF_SIZE, selectbuf );
+    glRenderMode( GL_SELECT );
+    glGetError();
+
+    delete picked_objects;
+    picked_objects = new vector< PickData > ();
+}
+
+void GFXSetPickName( int name )
+{
+    glInitNames();
+    glPushName( name );
+    glLoadName( name );
+    glGetError();
+}
+
+//Don't call this function
+bool GFXCheckPicked()
+{
+    assert( 0 );
+    glFlush();
+    int num_hits = glRenderMode( GL_RENDER );
+    glRenderMode( GL_SELECT );
+    glGetError();
+    assert( num_hits >= 0 && num_hits <= 1 );
+    return num_hits > 0;
+}
+
+vector< PickData > * GFXEndPick()
+{
+    int num_hits  = glRenderMode( GL_RENDER );
+    assert( num_hits != -1 );
+    //num_hits could == -1, check for this case
+    unsigned *ptr = selectbuf;
+    for (int a = 0; a < num_hits; a++) {
+        picked_objects->push_back( PickData( ptr[3], ptr[1], ptr[2] ) );
+        ptr += ptr[0]+3;
+    }
+    //restore gl projection matrix
+    glMatrixMode( GL_PROJECTION );
+    glPopMatrix();
+    return picked_objects;
+}
+
